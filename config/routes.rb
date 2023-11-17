a03843ae43f80c974d1196fd9618c9b32d178ec5
nakharinh francois-xavier phomsouvanh <elektrodynamiks@me.com>
2023-11-17 14:43:58 +1100

    routes and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  devise_for :users
 
+  devise_for :users
+  root to: "pages#home"
+  get 'pages/test', to: "pages#test", as: :test
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
