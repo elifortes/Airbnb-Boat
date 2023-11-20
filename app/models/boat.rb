8f1d77e2c1a7ecdab2b9022f8ac80f5f04de6f85
menahil liaqat <zoyagee786@gmail.com>
2023-11-20 17:46:43 +1100

    Added validation and attached photos option for cloudiunary

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,11 @@
 class Boat < ApplicationRecord
   belongs_to :user
+
+  validates :availability_from, :availability_to, future_date: true
+  validates :price_per_unit, :reviews, :guest_capacity, :year_made, numericality: { greater_than: 0 }
+  validates :title, :description, :price_per_unit, presence: true
+  
+  has_many_attached :photos
 end
+
+# validates :title, :description, :price_per_unit, :captain_name, :guest_capacity, :availability_from, :availability_to, :boat_maker_name, :boat_model, :boat_size, :year_made, presence: true
