04fefda669bfbdd79af735ca81f7e958b8631f58
Eliane Fortes <elimrfortes@gmail.com>
2023-11-20 15:33:58 +1100

    update-controller

---
+++ test/controllers/bookings_controller_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class BookingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def index
+  end
+end
