cf48b8f0528c30bd02725800d8675d0bd956ab81
nakharinh francois-xavier phomsouvanh <elektrodynamiks@me.com>
2023-11-20 19:18:05 +1100

    add_references :boat not add_reference :boats in migration file

--- db/migrate/20231120061429_add_name_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :phone_number, :string
-    add_column :users, :address, :string
-    add_column :users, :full_name, :string
-  end
-end
