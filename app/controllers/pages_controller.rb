44fdb373de3ca832fc704cd226eee3a230a09da7
nakharinh francois-xavier phomsouvanh <elektrodynamiks@me.com>
2023-11-21 11:07:36 +1100

    pages controller devise action before run

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home test nktest components]
+  # skip_before_action :authenticate_user!, only: %i[home test nktest components]
   def home
     @user = current_user
   end
