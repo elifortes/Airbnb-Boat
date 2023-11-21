d5d099b29143ed1404beccf23410e7ecee5e5bb9
Eliane Fortes <elimrfortes@gmail.com>
2023-11-21 14:58:54 +1100

    update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,58 @@
 class BookingsController < ApplicationController
+  # before_action :authorize_user!, only: [:edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+    @bookings = @boat.bookings
+  end
+
+
+  def new
+    @booking = Booking.new
+    # @boats = Boat.all
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    # @booking.user = current_user
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: 'Booking was successfully updated.'
+    else
+      @boats = Boat.all
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to user_path(@booking.user), notice: 'Booking was successfully destroyed.'
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:boat_id, :user_id, :status, :start_date, :end_date, :total_price)
+  end
+
+  # def authorize_user!
+  #   redirect_to root_path, unless @booking.user == current_user
+  # end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_220221) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_110501) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,14 +73,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_220221) do
     t.bigint "boat_id", null: false
     t.index ["boat_id"], name: "index_bookings_on_boat_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
-
-  create_table "monuments", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.date "opening_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
