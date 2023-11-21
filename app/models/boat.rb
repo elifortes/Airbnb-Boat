f86475b66147a1f55994aecd110359dc48204bc6
menahil liaqat <zoyagee786@gmail.com>
2023-11-21 16:48:08 +1100

    added the calender and uplaod image logic

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,6 +3,11 @@ class Boat < ApplicationRecord
   # validates :price_per_unit, :reviews, :year_made, numericality: { greater_than: 0 }
   # validates :title, :description, :price_per_unit, presence: true
 
+  # validates :availability_from, :availability_to, presence: true, if: :date_range
+  # def date_range
+  #   availability_from.present? && availability_to.present? && availability_from < availability_to
+  # end
+
   has_many_attached :photos
 end
 
