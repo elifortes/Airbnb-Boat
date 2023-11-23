3cdee48a84204de07e4089acf2a25bf28905aff2
Eliane Fortes <elimrfortes@gmail.com>
2023-11-23 17:05:00 +1100

    ...

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -37,7 +37,7 @@ class BoatsController < ApplicationController
     @boat = Boat.new(boat_params)
     @boat.user = @user
     if @boat.save
-      redirect_to edit_boat_path, notice: 'Boat ad was successfully created.'
+      redirect_to new_boat_path, notice: 'Boat ad was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
