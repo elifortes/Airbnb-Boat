66480b9ebcb62457c15af1859c86ed3b9c2961b9
nakharinh francois-xavier phomsouvanh <elektrodynamiks@me.com>
2023-11-23 20:12:09 +1100

    update doesnt remove all pics

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,26 +2,7 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  #  searchkick
-
-  # validates :price_per_unit, :year_made, :guest_capacity, numericality: { greater_than: 0 }
-  # validates :title, :description, :price_per_unit, presence: true
-
-
-  validates :title, :description, :price_per_unit, :captain_name, :guest_capacity, :availability_from, :availability_to, :boat_maker_name, :boat_model, :boat_size, :year_made, presence: true
-  # validate :date_range
-
-
-  # validates :price_per_unit, :reviews, :year_made, numericality: { greater_than: 0 }
-  # validates :title, :description, :price_per_unit, presence: true
-
-
-  # validates :availability_from, :availability_to, presence: true, if: :date_range
-  # def date_range
-  #   availability_from.present? && availability_to.present? && availability_from < availability_to
-  # end
-
-
+  validates :title, :description, :price_per_unit, :guest_capacity, :availability_from, :availability_to, presence: true
 
   def date_range
     if availability_from.present? && availability_to.present?
@@ -34,16 +15,13 @@ class Boat < ApplicationRecord
     end
   end
 
-
-
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   has_many_attached :photos, dependent: :destroy
 
-
-include PgSearch::Model
+  include PgSearch::Model
   pg_search_scope :search_boats,
-                  against: [:title, :price_per_unit, :captain_name, :guest_capacity, :location],
+                  against: %i[title price_per_unit captain_name guest_capacity location],
                   using: {
                     tsearch: { prefix: true }
                   }
