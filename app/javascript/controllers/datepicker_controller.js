40e0c7a14213cb8d7acfe54b662ff0d0f8b81e12
menahil liaqat <zoyagee786@gmail.com>
2023-11-24 12:51:24 +1100

    added the enable/disable functionality to the calender

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,7 +4,22 @@ import flatpickr from "flatpickr";
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element);
-    // console.log(this.element)
+
+    const minDate = this.element.getAttribute("data-min-date");
+    const maxDate = this.element.getAttribute("data-max-date");
+
+    flatpickr(this.element, {
+      minDate: minDate,
+      maxDate: maxDate,
+      // Add any other options you need here
+    });
+    // if (this.element.hasAttribute("data-enable-dates")) {
+    //   const enableDates = JSON.parse(this.element.getAttribute("data-enable-dates"));
+    //   flatpickr(this.element, {
+    //     enable: enableDates,
+    //   });
+    // } else {
+    //   flatpickr(this.element);
+    // }
   }
 }
